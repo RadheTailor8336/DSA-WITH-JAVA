32915ff48fe1b2e5f2bdbcc054cbe8ec94fcd738
RadheTailor8336 <radhetailor923@gmail.com>
2025-08-27 19:10:12 +0530

    Updated maxinArray.java file

--- src/Arrays/maxinArray.java
+++ src/Arrays/maxinArray.java
@@ -3,32 +3,34 @@ import java.util.Scanner;
 public class maxinArray {
     public static void main(String[] args) {
 
-//        int[] arr = {10,8,430,12,5,56,3};
+     int[] arr = {10,8,430,12,5,56,3};
+    // arr[7] =12;  // it gives error cause of index out of bound
+
 //        int n = arr.length;
 //        int mx = Integer.MIN_VALUE; // Integer.MIN_VALUE = -2147483648
 //        for(int i =1; i < n; i++){
 //          //  if(arr[i] > mx) mx = arr[i];
-//            mx = Math.max(mx,arr[i]);
+//            mx = Math.max(mx,arr[i]);git status
 //
 //        }
 //        System.out.println(mx);
 
-        int[] arr = {10,8,430,12,5,56,3};
-        int n = arr.length;
-        int mx = Integer.MIN_VALUE; // Integer.MIN_VALUE = -2147483648
-        for(int i =1; i < n; i++){
-            //  if(arr[i] > mx) mx = arr[i];
-            mx = Math.max(mx,arr[i]);
-
-        }
-        int smx = Integer.MIN_VALUE;
-        for(int i =1; i < n; i++){
-             if(arr[i] != mx)
-            smx = Math.max(smx,arr[i]);
-
-        }
-        System.out.println(mx);
-        System.out.println(smx);
+//        int[] arr = {10,8,430,12,5,56,3};
+//        int n = arr.length;
+//        int mx = Integer.MIN_VALUE; // Integer.MIN_VALUE = -2147483648
+//        for(int i =1; i < n; i++){
+//            //  if(arr[i] > mx) mx = arr[i];
+//            mx = Math.max(mx,arr[i]);
+//
+//        }
+//        int smx = Integer.MIN_VALUE;
+//        for(int i =1; i < n; i++){
+//             if(arr[i] != mx)
+//            smx = Math.max(smx,arr[i]);
+//
+//        }
+//        System.out.println(mx);
+//        System.out.println(smx);
     }
     }
 
