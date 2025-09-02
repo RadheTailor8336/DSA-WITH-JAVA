63f084eb57d20fd322e27c0c9a161a50db01f104
RadheTailor8336 <radhetailor923@gmail.com>
2025-09-02 19:00:08 +0530

    118. Pascal's Triangle Leetcode

--- src/TwoDimensionalArray/ArrayListsTwoD.java
+++ src/TwoDimensionalArray/ArrayListsTwoD.java
@@ -36,3 +36,29 @@ public class ArrayListsTwoD {
 
     }
 }
+
+
+/** 118. Pascal's Triangle Leetcode
+ *
+
+ * class Solution {
+ *     public List<List<Integer>> generate(int n) {
+ *         List<List<Integer>> ans = new ArrayList<>();
+ *         for(int i =0; i<n; i++){
+ *             List<Integer> l = new ArrayList<>();
+ *             for(int j = 0; j<=i; j++){
+ *                if(j ==0 || j == i) l.add(1);
+ *
+ *             else{
+ *                 l.add(ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
+ *             }
+ *         }
+ *             ans.add(l);
+ *
+ *         }
+ *
+ *         return ans;
+ *     }
+ * }
+ *
+ *  */
